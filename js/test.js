3e1d3f971988301a195b1fd72f9a169ce7f368b2
RSynhane <ryanshen0922@gmail.com>
2018-09-03 22:33:34 +0800

    modulize

--- js/test.js
+++ js/test.js
@@ -1,24 +1,13 @@
 // Budget Controller
-var budgetController = (function() {
+var budgetController = (function () {
 
-    // Some Code
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    //Some code
 
 })();
 
 
 // UI Controller
-var UIController = (function(){
+var UIController = (function () {
 
     // Some Code
 
@@ -26,14 +15,33 @@ var UIController = (function(){
 
 
 // Global App Controller
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget Controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log('It works.');
     }
 
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+
+        if(event.keycode === 13 || event.which === 13 ) {
+            ctrlAddItem();
+        }
+
+    });
+
 })(budgetController, UIController);
+
+
